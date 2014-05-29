5bf07fcd1b114e8d9c6b51e0fa2b761a4a3069f0
Phantom_64 <Phantom-64@users.noreply.github.com>
2014-05-29 12:02:07 +0530

    Merged BlockBreak with BlockPlace.

--- src/me/phantom64/ffa/listeners/BlockListener.java
+++ src/me/phantom64/ffa/listeners/BlockListener.java
@@ -4,11 +4,19 @@ import me.phantom64.ffa.utils.GameManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 
-public class BlockBreak implements Listener {
+public class BlockListener implements Listener {
 
     @EventHandler
     public void onBreak (BlockBreakEvent e) {
+        if (GameManager.isPlaying(e.getPlayer())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPlace (BlockPlaceEvent e) {
         if (GameManager.isPlaying(e.getPlayer())) {
             e.setCancelled(true);
         }
